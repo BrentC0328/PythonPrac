7ff4090c8e0e3bda70f45cda12fb57520d9badb2
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-11 15:55:57 -0400

    first commit

---
+++ Hello.py
@@ -0,0 +1,21 @@
+print ("\n" "\n")
+
+# Hello World
+# print('Hello World!') 
+
+# Declare Variables
+# x = 1 
+# x += 2 
+# print(x) 
+
+
+# (def)ine a function
+def add_stuff(x, y):
+    print(x + y)
+
+add_stuff(5, 7)
+
+
+
+
+print ("\n" "\n")
---
+++ WeLoopin.py
@@ -0,0 +1,18 @@
+print ("\n" "\n")
+
+def factorial(x):
+    factor = 1
+    for num in range(1, x + 1):
+        factor *= num
+    return factor
+    
+
+one_hundred_twenty = factorial(5)
+
+print(one_hundred_twenty)
+
+
+
+
+
+print ("\n" "\n")
